4bcb05ba4788b7b3db98f6ac7116136dc8c6d9d8
Williman420 <dwikaariana@gmail.com>
2026-05-22 11:18:14 +0800

    tables created

---
+++ database/migrations/2026_05_22_031747_create_subscriptions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('customer_id')
+                ->constrained('customers')
+                ->onDelete('cascade');
+
+            $table->foreignId('service_id')
+                ->constrained('services')
+                ->onDelete('cascade');
+
+            $table->date('start_date');
+
+            $table->date('end_date')->nullable();
+
+            $table->enum('status', [
+                'active',
+                'inactive',
+                'trial',
+                'isolir',
+                'dismantle'
+            ]);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
