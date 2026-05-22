3666c6a8171d450fe4b9db61eee2067049344ab9
Williman420 <dwikaariana@gmail.com>
2026-05-22 15:34:29 +0800

    merge complete

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\SubscriptionController;
+use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\CustomerController;
+
+
+
+Route::apiResource("services", ServiceController::class);
+Route::patch("services/{service}/activate", [
+    ServiceController::class,
+    "activate",
+]);
+Route::patch("services/{service}/deactivate", [
+    ServiceController::class,
+    "deactivate",
+]);
+Route::apiResource('subscriptions', SubscriptionController::class)
+    ->only([
+        'index',
+        'store',
+        'show',
+        'update'
+    ]);
+
+
 
 Route::apiResource('customers', CustomerController::class);
 
