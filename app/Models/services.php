e70146af345140a44acf17dc3b059a472a57060c
Williman420 <dwikaariana@gmail.com>
2026-05-22 11:36:16 +0800

    service controller and route

--- app/Models/services.php
+++ app/Models/services.php
@@ -1,10 +1,29 @@
 <?php
 
+
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Subscriptions;
 
-class services extends Model
+class Service extends Model
 {
-    //
+    protected $fillable = ["name", "price", "description", "status"];
+    protected function casts(): array
+    {
+        return [
+            "status" => "boolean",
+            "price" => "integer",
+        ];
+    }
+    /**
+     * @return HasMany<Subscriptions, $this>
+     */
+    public function subscriptions(): HasMany
+    {
+        return $this->hasMany(Subscriptions::class);
+    }
 }
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\ServiceController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource("services", ServiceController::class);
+Route::patch("services/{service}/activate", [
+    ServiceController::class,
+    "activate",
+]);
+Route::patch("services/{service}/deactivate", [
+    ServiceController::class,
+    "deactivate",
+]);
